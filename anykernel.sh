f72cef2f336a75352bf6c0cf42241f87459804a9
Eliminater74 <eliminater74@gmail.com>
2017-02-01 20:54:42 -0500

    Nebula: Advanced Edition: Update
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -11,6 +11,7 @@ do.cleanup=1
 do.fix_pnpmgr=0
 do.pnpmgr=1
 do.fix_pnpmgr_ramdisk=0
+do.eas_support=0
 do.boost_scripts=0
 do.cmdlinestr=0
 do.system_blobs=0
@@ -41,11 +42,65 @@ dump_boot;
 
 # begin ramdisk changes
 
+#### Ramdisk Changes For EAS Kernel Only. Stock Based Mostly ####
+
 insert_line init.rc "import /init.nebula.rc" after "import /init.power.rc" "import /init.nebula.rc";
+insert_line init.rc "    mkdir /dev/stune/background" after "    mount cgroup none /dev/stune schedtune" "    mkdir /dev/stune/background"
+insert_line init.rc "    mkdir /dev/stune/top-app" after "    mkdir /dev/stune/foreground" "    mkdir /dev/stune/top-app"
+insert_line init.rc "    chown system system /dev/stune/background" after "    chown system system /dev/stune" "    chown system system /dev/stune/background"
+insert_line init.rc "    chown system system /dev/stune/top-app" after "    chown system system /dev/stune/foreground" "    chown system system /dev/stune/top-app"
+insert_line init.rc "    chown system system /dev/stune/background/tasks" after "    chown system system /dev/stune/tasks" "    chown system system /dev/stune/background/tasks"
+insert_line init.rc "    chown system system /dev/stune/top-app/tasks" after "    chown system system /dev/stune/foreground/tasks" "    chown system system /dev/stune/top-app/tasks"
+insert_line init.rc "    chmod 0664 /dev/stune/background/tasks" after "    chmod 0664 /dev/stune/tasks" "    chmod 0664 /dev/stune/background/tasks"
+insert_line init.rc "    chmod 0664 /dev/stune/top-app/tasks" after "    chmod 0664 /dev/stune/foreground/tasks" "    chmod 0664 /dev/stune/top-app/tasks"
+insert_line init.rc "    seclabel u:r:init:s0" before "    class main" "    seclabel u:r:init:s0"
+insert_line init.rc "    seclabel u:r:init:s0" after "service usbdiag_init  /system/bin/sh /init.usbdiag.sh" "    seclabel u:r:init:s0"
+insert_line init.rc "    mkdir" after "    mkdir" "    mkdir"
+insert_line init.power.rc "    seclabel u:r:init:s0" after "service setfps /system/bin/sh /system/etc/setfps.sh" "    seclabel u:r:init:s0"
+insert_line init.power.rc "    seclabel u:r:init:s0" after "service setFOTA /system/bin/sh /system/etc/setFOTAfreq.sh" "    seclabel u:r:init:s0"
+
+remove_section init.power.rc "#CPUSET" "top-app/cpus";
+remove_section init.power.rc "# init PnPMgr node" "200";
+remove_section init.power.rc "property:init.svc.thermal-engine=stopped" "/sys/power/pnpmgr/cluster/little/cpu3/thermal_freq";
+remove_section init.power.rc "service pnpmgr" "root";
+remove_section init.power.rc "thermal-engine=stopped" "little/cpu3/thermal_freq"
+remove_section init.rc "# Reload policy from /data/security if present." "setprop selinux.reload_policy 1"
+
+replace_line init.zygote64_32.rc "    writepid /dev/cpuset/foreground/tasks /sys/fs/cgroup/stune/foreground/tasks" "    writepid /dev/cpuset/foreground/tasks /dev/stune/foreground/tasks"
+replace_line init.zygote32.rc "    writepid /dev/cpuset/foreground/tasks /dev/stune/foreground/tasks" "    writepid /dev/cpuset/foreground/tasks"
+### Stop texfat from starting at this level ###
+remove_line init.htc.storage.exfat.rc "    insmod /system/lib/modules/texfat.ko"
+
 
 # end ramdisk changes
 
 write_boot;
 
 ## end install
+# dump_boot
 
+# backup_file <file>
+
+# replace_string <file> <if search string> <original string> <replacement string>
+
+# replace_section <file> <begin search string> <end search string> <replacement string>
+
+# remove_section <file> <begin search string> <end search string>
+
+# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
+
+# replace_line <file> <line replace string> <replacement line>
+
+# remove_line <file> <line match string>
+
+# prepend_file <file> <if search string> <patch file>
+
+# insert_file <file> <if search string> <before|after> <line match string> <patch file>
+
+# append_file <file> <if search string> <patch file>
+
+# replace_file <file> <permissions> <patch file>
+
+# patch_fstab <fstab file> <mount match name> <fs match type> <block|mount|fstype|options|flags> <original string> <replacement string>
+
+# write_boot
